6962fa72499ef53351b29ad7b4fe1f5ea466c519
shreyajp <shreyajp6@gmail.com>
2025-08-28 21:34:14 +0930

    save

--- Finder.cpp
+++ Finder.cpp
@@ -1,17 +1,14 @@
 #include "Finder.h"
-#include <string>
-#include <vector>
 
 std::vector<int> Finder::findSubstrings(std::string s1, std::string s2) {
     std::vector<int> result;
     if (s2.empty()) return result;
 
     std::string prefix;
-    std::size_t start = 0;           
-                                    
+    std::size_t start = 0;
+
     for (std::size_t i = 0; i < s2.size(); ++i) {
         prefix.push_back(s2[i]);
-
         std::size_t pos = s1.find(prefix, start);
 
         if (pos == std::string::npos) {
@@ -20,9 +17,8 @@ std::vector<int> Finder::findSubstrings(std::string s1, std::string s2) {
             break;
         } else {
             result.push_back(static_cast<int>(pos));
-            start = pos;
+            start = pos; 
         }
     }
-
     return result;
 }
--- Finder.h
+++ Finder.h
@@ -6,7 +6,6 @@
 
 class Finder {
 public:
-   
     std::vector<int> findSubstrings(std::string s1, std::string s2);
 };
 
