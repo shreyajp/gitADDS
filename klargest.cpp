66a98f9149f52807c15248b40df288fbee6f0df4
shreyajp <shreyajp6@gmail.com>
2025-10-23 13:15:23 +1030

    save

---
+++ klargest.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int kth_largest(vector<int> values, int k) {
+
+    
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    
+    for (int val : values) {
+        minHeap.push(val);
+        
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+    }
+    
+    return minHeap.top();
+}
