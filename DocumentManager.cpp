65df18d0198017b4a4576dfe77c7f55df7e8d9fe
shreyajp <shreyajp6@gmail.com>
2025-10-23 13:49:40 +1030

    save

--- DocumentManager.cpp
+++ DocumentManager.cpp
@@ -1,88 +1,57 @@
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <map>
+#include "DocumentManager.h"
 
-using namespace std;
+void DocumentManager::addDocument(string name, int id, int license_limit) {
+    Document doc(name, id, license_limit);
+    documentsById[id] = doc;
+    documentsByName[name] = id;
+}
 
-class DocumentManager {
-private:
-    struct Document {
-        string name;
-        int id;
-        int licenseLimit;
-        int currentBorrowed;
-        
-        Document() : name(""), id(0), licenseLimit(0), currentBorrowed(0) {}
-        Document(string n, int i, int l) : name(n), id(i), licenseLimit(l), currentBorrowed(0) {}
-    };
-    
+void DocumentManager::addPatron(int patronID) {
+    patrons.insert(patronID);
+}
 
-    unordered_map<int, Document> documentsById;
-    
-    unordered_map<string, int> documentsByName;
-    
-    unordered_set<int> patrons;
-    
+int DocumentManager::search(string name) {
+    auto it = documentsByName.find(name);
+    if (it != documentsByName.end()) {
+        return it->second;
+    }
+    return 0;
+}
 
-    unordered_map<int, unordered_set<int>> borrowedBy;
-
-public:
-    void addDocument(string name, int id, int license_limit) {
-        Document doc(name, id, license_limit);
-        documentsById[id] = doc;
-        documentsByName[name] = id;
+bool DocumentManager::borrowDocument(int docId, int patronID) {
+    if (patrons.find(patronID) == patrons.end()) {
+        return false;
     }
     
-    void addPatron(int patronID) {
-        patrons.insert(patronID);
+    if (documentsById.find(docId) == documentsById.end()) {
+        return false;
     }
     
+    Document& doc = documentsById[docId];
+    
+    if (borrowedBy[docId].find(patronID) != borrowedBy[docId].end()) {
+        return false;
+    }
+    
+    if (doc.currentBorrowed >= doc.licenseLimit) {
+        return false;
+    }
+    
+    doc.currentBorrowed++;
+    borrowedBy[docId].insert(patronID);
+    return true;
+}
 
-    int search(string name) {
-        auto it = documentsByName.find(name);
-        if (it != documentsByName.end()) {
-            return it->second;
-        }
-        return 0;
+void DocumentManager::returnDocument(int docId, int patronID) {
+    if (documentsById.find(docId) == documentsById.end()) {
+        return;
     }
     
-
-    bool borrowDocument(int docId, int patronID) {
-        if (patrons.find(patronID) == patrons.end()) {
-            return false;
-        }
-        
-        if (documentsById.find(docId) == documentsById.end()) {
-            return false;
-        }
-        
-        Document& doc = documentsById[docId];
-        
-        if (borrowedBy[docId].find(patronID) != borrowedBy[docId].end()) {
-            return false; 
-        }
-        
-        if (doc.currentBorrowed >= doc.licenseLimit) {
-            return false; 
-        }
-        
-        doc.currentBorrowed++;
-        borrowedBy[docId].insert(patronID);
-        return true;
+    if (borrowedBy[docId].find(patronID) == borrowedBy[docId].end()) {
+        return;
     }
     
-    void returnDocument(int docId, int patronID) {
-        if (documentsById.find(docId) == documentsById.end()) {
-            return;
-        }
-        
-        if (borrowedBy[docId].find(patronID) == borrowedBy[docId].end()) {
-            return; 
-        }
-        
-        Document& doc = documentsById[docId];
-        doc.currentBorrowed--;
-        borrowedBy[docId].erase(patronID);
-    }
-};
+    Document& doc = documentsById[docId];
+    doc.currentBorrowed--;
+    borrowedBy[docId].erase(patronID);
+}
--- DocumentManager.h
+++ DocumentManager.h
@@ -15,30 +15,20 @@ private:
         int licenseLimit;
         int currentBorrowed;
         
-        Document();
-        Document(string n, int i, int l);
+        Document() : name(""), id(0), licenseLimit(0), currentBorrowed(0) {}
+        Document(string n, int i, int l) : name(n), id(i), licenseLimit(l), currentBorrowed(0) {}
     };
     
-
     unordered_map<int, Document> documentsById;
-    
     unordered_map<string, int> documentsByName;
-    
     unordered_set<int> patrons;
-    
     unordered_map<int, unordered_set<int>> borrowedBy;
 
 public:
     void addDocument(string name, int id, int license_limit);
-    
     void addPatron(int patronID);
-    
-
     int search(string name);
-    
-
     bool borrowDocument(int docId, int patronID);
-    
     void returnDocument(int docId, int patronID);
 };
 
